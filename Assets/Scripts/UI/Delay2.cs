351ff35bc724869e0ca396e8c479f8e82825ed2f
YingHen1104 <yinghen02@gmail.com>
2024-08-21 13:17:38 +0800

    return to song or retry

---
+++ Assets/Scripts/UI/Delay2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delay2 : MonoBehaviour
+{
+	public GameObject button1;
+	public GameObject button2;
+	public GameObject text1;
+	public GameObject text2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnDelay", 98);
+    }
+
+    // Update is called once per frame
+    private void SpawnDelay()
+    {
+        button1.SetActive(true);
+		button2.SetActive(true);
+		text1.SetActive(true);
+		text2.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UI/Delay3.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delay3 : MonoBehaviour
+{
+	public GameObject button1;
+	public GameObject button2;
+	public GameObject text1;
+	public GameObject text2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnDelay", 100);
+    }
+
+    // Update is called once per frame
+    private void SpawnDelay()
+    {
+        button1.SetActive(true);
+		button2.SetActive(true);
+		text1.SetActive(true);
+		text2.SetActive(true);
+    }
+}
